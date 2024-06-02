f15c9845924f680d99334132552669d5389b64b6
khoi1235612 <148308624+khoi1235612@users.noreply.github.com>
2024-06-02 10:49:42 +0700

    countSystem.c

--- countSystem.c
+++ countSystem.c
@@ -5,11 +5,13 @@
  *      Author: Windows 10 TIMT
  */
 #include "countSystem.h"
+
 void wrapAroundCounter(){
 	if (n > 9) n=0;
 	if (n < 0) n=9;
 }
 int n = 0;
+
 void countSystem(){
 	if (is_button_pressed(0) == 1){
 		n = 0;
@@ -27,9 +29,18 @@ void countSystem(){
 		HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin);
 		setTimer0(100);
 	}
-	if (timer1_flag == 1){
+
+	if (timer1_flag == 1) {
+			if (is_button_pressed(0) == 0 && is_button_pressed(1) == 0 && is_button_pressed(2) == 0) {
+				n--;
+				if (n < 0) n=0;
+				setTimer1(1000);
+			}
+	}
+
+	if (timer2_flag == 1){
 		display7SEG(n);
-		setTimer1(1);
+		setTimer2(1);
 	}
 
 }
